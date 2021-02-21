8086503ddbaff94e04d49a381f77af148d3992ee
Jonathan <jonathan.dupau24@gmail.com>
2021-02-21 01:24:08 +0100

    add name of character on quote

--- src/js/main.js
+++ src/js/main.js
@@ -5,18 +5,32 @@ const numberGenerator = () => {
     return randomNumber = "5cd96e05de30eff6ebcce" + random.toString();
 };
 numberGenerator();
+
 let url = "https://the-one-api.dev/v2/quote/";
 let urlRandom = url + randomNumber;
-
 const getQuote = () => {
     fetch(urlRandom  , {method:'GET',
     headers: {'Authorization': 'Bearer '+ 'rQbJd3RkXyanIew_vasd'}
    })
     .then((res) => res.json())
     .then((data) => {
-        quote.innerHTML = `<cite>"${data.docs[0].dialog}"</cite>`;
+        quote.innerHTML = `<cite>"${data.docs[0].dialog}"</cite><span id="author"></span>`;
+        const author = document.getElementById('author')
+        let character = data.docs[0].character;
+        console.log(typeof character);
+        let urlCharacter = "https://the-one-api.dev/v2/character/" + character;
+        console.log(urlCharacter);
+        fetch(urlCharacter  , {method:'GET',
+        headers: {'Authorization': 'Bearer '+ 'rQbJd3RkXyanIew_vasd'}
+       })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data.docs[0].name);
+            characterName = data.docs[0].name;
+            author.innerHTML = characterName;
+        });
     });  
-}; 
+}
 getQuote();
 quote.addEventListener('click', () => {
     getQuote();
