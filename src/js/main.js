fce35c720d1db92ea1b938fc194a1c82afe183e9
Jonathan <jonathan.dupau24@gmail.com>
2021-02-21 13:21:59 +0100

    fix function for click function

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,11 @@
 const quote = document.getElementById('quote');
 
-const numberGenerator = () => {  
-    let random = Math.floor(Math.random() * (1000 - 900)) + 900; 
-    return randomNumber = "5cd96e05de30eff6ebcce" + random.toString();
-};
-numberGenerator();
 
-let url = "https://the-one-api.dev/v2/quote/";
-let urlRandom = url + randomNumber;
 const getQuote = () => {
+    let random = Math.floor(Math.random() * (1000 - 900)) + 900; 
+    randomNumber = "5cd96e05de30eff6ebcce" + random.toString();
+    let url = "https://the-one-api.dev/v2/quote/";
+    let urlRandom = url + randomNumber;
     fetch(urlRandom  , {method:'GET',
     headers: {'Authorization': 'Bearer '+ 'rQbJd3RkXyanIew_vasd'}
    })
